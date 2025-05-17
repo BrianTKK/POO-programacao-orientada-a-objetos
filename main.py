e002578641ff79fa8ca6efda6eec0597768022eb
Brian <brianhenriquedp@gmail.com>
2025-05-17 18:19:26 -0400

    feat(1.1): Implementa encapsulamento no atributo combustivel da classe Carro
    Altera o atributo `combustivel` para privado (`__combustivel`)
    e adiciona propriedades com `@property` e `@combustivel.setter`
    para controlar o acesso e a modificação do nível de combustível,
    melhorando a manutenibilidade e a segurança da classe.

--- main.py
+++ main.py
@@ -3,24 +3,37 @@ try:
         def __init__(self, modelo, fabricante):
             self.modelo = modelo
             self.fabricante = fabricante
-            self.combustivel = 100
+            self.__combustivel = 100
+        
+        @property
+        def combustivel(self):
+            return self.__combustivel
+
+        @combustivel.setter
+        def combustivel(self, valor):
+            if valor < self.__combustivel:
+                print("Valor de combustível mínimo invalido")
+            else:
+                self.__combustivel = valor
 
         def acelerar(self):
-            if self.combustivel <= 0:
+            if self.__combustivel <= 0:
                 print("Sem combustivel")
             else:
-                self.combustivel -= 10
+                self.__combustivel -= 10
                 print("Acelerando...")
+
 
     meu_carro = Carro("Opala", "Chevrolet")
 
     print(f"Fabricante: {meu_carro.fabricante}")
     print(f"Modelo: {meu_carro.modelo}")
-    print(f"Combustivel: {meu_carro.combustivel}")
+    print(f"Combustivel: {meu_carro.__combustivel}")
 
     meu_carro.acelerar()
+
     print(f"Fabricante: {meu_carro.fabricante}")
     print(f"Modelo: {meu_carro.modelo}")
-    print(f"Combustivel: {meu_carro.combustivel}")
+    print(f"Combustivel: {meu_carro.__combustivel}")
 except Exception as e:
     print(f"Erro: {e}")
